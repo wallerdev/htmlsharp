4e33705f64daf26c23017deb1c5f668a1b706707
Edward Waller <wallerdev@gmail.com>
2009-01-10 23:47:46 -0500

    Fixed assertequal parameters, and added quoted attribute test

--- HtmlSharp.Tests/HtmlParserTests.cs
+++ HtmlSharp.Tests/HtmlParserTests.cs
@@ -24,98 +24,108 @@ namespace HtmlSharp.Tests
         {
             parser = new HtmlParser();
             var page = parser.Parse("<tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestSingleTagWithoutContentAndWithClosingTag()
         {
             var page = parser.Parse("<tag></tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestSingleTagWithoutContentAndWithSelfClosingTag()
         {
             var page = parser.Parse("<tag/>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestSingleTagWithContent()
         {
             var page = parser.Parse("<tag>content");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag", new HtmlText() { Value = "content" })));
+            Assert.AreEqual(new Root(new UnknownTag("tag", new HtmlText() { Value = "content" })), page.Root);
         }
 
         [Test]
         public void TestSingleTagWithContentAndWithClosingTag()
         {
             var page = parser.Parse("<tag>content</tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag", new HtmlText() { Value = "content" })));
+            Assert.AreEqual(new Root(new UnknownTag("tag", new HtmlText() { Value = "content" })), page.Root);
         }
 
         [Test]
         public void TestRepeatedTagsWithoutContent()
         {
             var page = parser.Parse("<tag><tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag"), new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag"), new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestRepeatedTagsWithoutContentAndWithClosingTag()
         {
             var page = parser.Parse("<tag></tag><tag></tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag"), new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag"), new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestRepeatedTagsWithContent()
         {
             var page = parser.Parse("<tag>content<tag>content");
-            Assert.AreEqual(page.Root, new Root(
-                new UnknownTag("tag", new HtmlText() { Value = "content" }),
-                new UnknownTag("tag", new HtmlText() { Value = "content" })));
+            Assert.AreEqual(
+                new Root(
+                    new UnknownTag("tag", new HtmlText() { Value = "content" }),
+                    new UnknownTag("tag", new HtmlText() { Value = "content" })), page.Root);
         }
 
         [Test]
         public void TestRepeatedTagsWithContentAndWithClosingTags()
         {
             var page = parser.Parse("<tag>content</tag><tag>content</tag>");
-            Assert.AreEqual(page.Root, new Root(
-                new UnknownTag("tag", new HtmlText() { Value = "content" }),
-                new UnknownTag("tag", new HtmlText() { Value = "content" })));
+            Assert.AreEqual(
+                new Root(
+                    new UnknownTag("tag", new HtmlText() { Value = "content" }),
+                    new UnknownTag("tag", new HtmlText() { Value = "content" })), page.Root);
         }
 
         [Test]
         public void TestNestedTags()
         {
             var page = parser.Parse("<tag><innertag></tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag", new UnknownTag("innertag"))));
+            Assert.AreEqual(new Root(new UnknownTag("tag", new UnknownTag("innertag"))), page.Root);
         }
 
         [Test]
         public void TestNestedTagsWithContent()
         {
             var page = parser.Parse("<tag><innertag>content</tag>");
-            Assert.AreEqual(page.Root, new Root(
-                new UnknownTag("tag",
-                    new UnknownTag("innertag",
-                        new HtmlText() { Value = "content" }))));
+            Assert.AreEqual(
+                new Root(
+                    new UnknownTag("tag",
+                        new UnknownTag("innertag",
+                            new HtmlText() { Value = "content" }))), page.Root);
         }
 
         [Test]
         public void TestDuplicateRepeatedTags()
         {
             var page = parser.Parse("<tag><tag></tag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag"), new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag"), new UnknownTag("tag")), page.Root);
         }
 
         [Test]
         public void TestUnecessaryClosingTags()
         {
             var page = parser.Parse("<tag></tag></tag></innertag>");
-            Assert.AreEqual(page.Root, new Root(new UnknownTag("tag")));
+            Assert.AreEqual(new Root(new UnknownTag("tag")), page.Root);
+        }
+
+        [Test]
+        public void TestQuotedAttributes()
+        {
+            var page = parser.Parse("<div id=\"value\">");
+            Assert.AreEqual(new Root(new Div(new TagAttribute("id", "value"))), page.Root);
         }
 
         //Nestable Tags
@@ -125,7 +135,7 @@ namespace HtmlSharp.Tests
         {
             parser = new HtmlParser();
             var page = parser.Parse("<div><div></div>");
-            Assert.AreEqual(page.Root, new Root(new Div(new Div())));
+            Assert.AreEqual(new Root(new Div(new Div())), page.Root);
         }
     }
 }
