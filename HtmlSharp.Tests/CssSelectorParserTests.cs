eb023acd5b239113cf5b05f3918b72a64940abb7
Edward Waller <wallerdev@gmail.com>
2009-01-11 22:21:33 -0500

    Started work on the css selector parser and tests

---
+++ HtmlSharp.Tests/CssSelectorParserTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HtmlSharp.Elements.Tags;
+
+namespace HtmlSharp.Tests
+{
+    /// <summary>
+    /// Summary description for CssSelectorParserTests
+    /// </summary>
+    [TestClass]
+    public class CssSelectorParserTests
+    {
+        [TestMethod]
+        public void TestUniversalSelector()
+        {
+            CssSelectorParser parser = new CssSelectorParser();
+            var selector = parser.Parse("*");
+            Assert.AreEqual(new CssSelector(new CssUniversalSelector()), selector);
+        }
+
+        [TestMethod]
+        public void TestWhiteSpace()
+        {
+            CssSelectorParser parser = new CssSelectorParser();
+            var selectors = new[] { parser.Parse("a>b"), parser.Parse("a> b"), parser.Parse("a >b"), parser.Parse("a > b") };
+            foreach (var selector in selectors)
+            {
+                Assert.AreEqual(
+                    new CssSelector(
+                        new CssSimpleSelector[]
+                        {
+                            new CssTypeSelector(
+                                new P()), 
+                            new CssTypeSelector(
+                                new A())
+                        },
+                        new CssChildCombinator()), selector.ToString());
+            }
+        }
+    }
+}
---
+++ HtmlSharp/CssSelectorParser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HtmlSharp
+{
+    public class CssSelector
+    {
+        List<CssSimpleSelector> selectors = new List<CssSimpleSelector>();
+        List<CssCombinator> combinators = new List<CssCombinator>();
+
+        public void Add(CssSimpleSelector selector)
+        {
+            selectors.Add(selector);
+        }
+
+        public void Add(CssCombinator combinator)
+        {
+            combinators.Add(combinator);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder selectorBuilder = new StringBuilder();
+            for (int i = 0; i < selectors.Count; i++)
+            {
+                selectorBuilder.Append(selectors[i]);
+                if (combinators.Count > i)
+                {
+                    selectorBuilder.Append(combinators[i]);
+                }
+            }
+
+            return selectorBuilder.ToString();
+        }
+    }
+
+    public class CssCombinator
+    {
+
+    }
+
+    public class CssSimpleSelector
+    {
+
+    }
+
+    public class CssUniversalSelector : CssSimpleSelector
+    {
+        public override string ToString()
+        {
+            return "*";
+        }
+    }
+
+    public class CssSelectorParser
+    {
+        public CssSelector Parse(string selector)
+        {
+            CssSelector result = new CssSelector();
+
+            selector = RemoveUncessaryWhiteSpace(selector);
+
+            CssSimpleSelector currentSelector = null;
+
+            int i = 0;
+            while (i < selector.Length)
+            {
+                if (selector[i] == '*')
+                {
+                    currentSelector = new CssUniversalSelector();
+                }
+                i++;
+            }
+
+            result.Add(currentSelector);
+
+            return result;
+        }
+
+        string RemoveUncessaryWhiteSpace(string selector)
+        {
+            selector = Regex.Replace(selector, "[ \t\n\r\f]+", " ");
+            //selector = Regex.Replace(selector, " ?[
+
+            return selector;
+        }
+    }
+}
